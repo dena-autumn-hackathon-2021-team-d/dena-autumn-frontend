c813e4d99c733dda9f12eae2935b98f466ba7af3
shu-pf <shugoena@gmail.com>
2021-09-18 16:30:35 +0900

    トップページの作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>Top</h1>
+  <div>
+    <h1>質問でコミニケーションを引き起こそう！</h1>
+    <v-text-field v-model="groupName" label="グループ名"></v-text-field>
+    <v-btn color="primary" elevation="2" @click="create"
+      >グループを作成する</v-btn
+    >
+  </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      groupName: '',
+    }
+  },
+  methods: {
+    create() {
+      console.log('create')
+      this.$router.push('/groups/groupId/answers')
+    },
+  },
+})
+</script>

1782c0538df0767ebe1de3d0186de030b1500b32
shu-pf <shugoena@gmail.com>
2021-09-18 16:49:18 +0900

    ランディングページ用のレイアウト作成

---
+++ layouts/landing.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app dark>
+    <v-app-bar fixed app>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      title: 'Vuetify.js',
+    }
+  },
+})
+</script>

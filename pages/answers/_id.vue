22ac0a7434fff65fc934dc92cd4074580330d683
manattan <mogimogi0529@eis.hokudai.ac.jp>
2021-09-18 12:24:16 +0900

    コメントを送る機能を追加

--- pages/answers/_id.vue
+++ pages/answers/_id.vue
@@ -5,13 +5,23 @@
         <Anser />
       </div>
       <div class="detail-container__comments">
-        <div v-for="comment in comments" :key="comment.id" class="detail-container__comments__comment" >
+        <div
+          v-for="comment in comments"
+          :key="comment.id"
+          class="detail-container__comments__comment"
+        >
           <Comment :user="comment.user" :sentence="comment.sentence" />
         </div>
       </div>
     </div>
     <div class="new-question-container">
-    
+      <v-text-field
+        v-model="new_comment"
+        label="コメントを入力しよう！"
+        placeholder="いいね！"
+        outlined
+      ></v-text-field>
+      <v-btn @click="consol()" class="sendButton">コメントを送る</v-btn>
     </div>
   </section>
 </template>
@@ -20,23 +30,34 @@
 import Vue from 'vue'
 
 export default Vue.extend({
-  data () {
+  data() {
     return {
+      new_comment: '',
       comments: [
         {
-          'id': 1,
-          'user': 'manattan',
-          'sentence': 'わかる！'
+          id: 1,
+          user: 'manattan',
+          sentence: 'わかる！',
         },
         {
-          'id': 2,
-          'user': 'takapiro99',
-          'sentence': 'わからんな'
-        }
+          id: 2,
+          user: 'takapiro99',
+          sentence: 'わからんな',
+        },
       ],
-      answers: []
+      answers: [],
     }
-  }
+  },
+
+  methods: {
+    consol() {
+      this.comments.push({
+        id: this.comments.length + 1,
+        user: 'manattan',
+        sentence: this.new_comment,
+      })
+    },
+  },
 })
 </script>
 
@@ -47,6 +68,12 @@ export default Vue.extend({
       &__comment {
         margin: 12px 0;
       }
+    }
+  }
+  .new-question-container {
+    width: 100%;
+    .sendButton {
+      float: right;
     }
   }
 }
